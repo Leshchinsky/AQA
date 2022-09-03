da1fdfb8d83289c61a746d151647bd438e4da2d8
Dmitry Leshchinsky <dmitry.leshchinsky@gmail.com>
2022-09-03 21:18:03 +0200

    added files with lesson 1

---
+++ script.js
@@ -0,0 +1,45 @@
+let firstName
+let lastName
+firstName = "John"
+lastName = "Adams"
+
+document.write (firstName +" ", lastName, "<br>")
+console.log(firstName +" ", lastName)
+
+firstName = "Anna"
+lastName = "Carenina"
+
+document.write (firstName +" ", lastName, "<br>", "<br>", "<br>");
+console.log(firstName +" ", lastName);
+
+//let = fere
+let step1
+let step2
+let step3
+let step4
+let step5
+
+let ingridient1
+let ingridient2
+let ingridient3
+
+
+
+//fere = "Способ приготовления кофе"
+ingridient1 = "молотый кофе"
+ingridient2 = "воды"
+ingridient3 = "сливки"
+
+step1 = "Взять турку и положить " + ingridient1
+step2 = "Налить в турку "+ ingridient2
+step3 = "Кипятить до поднятия пены"
+step4 = "Перелить кофе в кружку"
+step5 = "Налить в кружку " + ingridient3 
+
+
+document.write(step1, "<br>", step2, "<br>", step3, "<br>", step4, "<br>", step5, "<br>")
+console.log(step1)
+console.log(step2)
+console.log(step3)
+console.log(step4)
+console.log(step5)
